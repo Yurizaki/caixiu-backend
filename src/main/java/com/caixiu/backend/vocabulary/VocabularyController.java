64001f70fa84e1e62bd172d6361059d9bf78d11a
Alex <alxdev94@gmail.com>
2022-05-23 23:30:41 +0100

    working import of db module via maven into backend

---
+++ src/main/java/com/caixiu/backend/vocabulary/VocabularyController.java
@@ -0,0 +1,24 @@
+package com.caixiu.backend.vocabulary;
+
+import com.caixiu.backend.user.model.User;
+import com.caixiu.backend.user.repository.UserRepository;
+//import com.holmes.crud.read.DataReader;
+//import com.holmes.vocabulary.Vocabulary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VocabularyController {
+
+//    @Autowired
+//    DataReader<Vocabulary> dataReader;
+//
+//
+//    @RequestMapping(value = "/vocabularies")
+//    public List<Vocabulary> getUser() {
+//        return dataReader.findAll();
+//    }
+}
