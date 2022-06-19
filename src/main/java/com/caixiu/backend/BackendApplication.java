90b138f596dbd09db82511af9e817304cd0fc5cd
Alex <alxdev94@gmail.com>
2022-06-19 21:05:35 +0100

    print running endpoints, prop file

--- src/main/java/com/caixiu/backend/BackendApplication.java
+++ src/main/java/com/caixiu/backend/BackendApplication.java
@@ -1,35 +1,58 @@
 package com.caixiu.backend;
 
 import com.holmes.aws.vocabulary.AwsDynamoDbVocabulary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.util.Map;
+
 @SpringBootApplication
 @Configuration
-public class BackendApplication implements CommandLineRunner {
+public class BackendApplication implements CommandLineRunner{
 
-	String host = "mongodb://localhost:27017";
-	String database = "CaixiuDB";
-	private DynamoDbClient dynamoDbClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);
+    public static final String API_VERSION_1_0 = "/v1.0";
 
-	@Bean
-	public AwsDynamoDbVocabulary awsDynamoDbVocabulary() {
-		dynamoDbClient = DynamoDbClient.builder()
-				.region(Region.EU_WEST_2)
-				.build();
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.EU_WEST_2)
+                .build();
+    }
 
-		return new AwsDynamoDbVocabulary(dynamoDbClient);
-	}
+    @Bean
+    public AwsDynamoDbVocabulary awsDynamoDbVocabulary(DynamoDbClient dynamoDbClient) {
+        return new AwsDynamoDbVocabulary(dynamoDbClient);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) { }
+    @Override
+    public void run(String... args) {
+        LOGGER.info("Starting up application");
+    }
+
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        RequestMappingHandlerMapping requestMappingHandlerMapping = applicationContext
+                .getBean("requestMappingHandlerMapping", RequestMappingHandlerMapping.class);
+        Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
+        map.forEach((key, value) -> LOGGER.info("{} {}", key, value));
+    }
 }
--- src/main/java/com/caixiu/backend/vocabulary/VocabularyController.java
+++ src/main/java/com/caixiu/backend/vocabulary/VocabularyController.java
@@ -16,44 +16,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.caixiu.backend.BackendApplication.API_VERSION_1_0;
+
 @RestController
 public class VocabularyController {
+
+    public static final String ENDPOINT_NAME_1_0 = "/vocabularies";
+    public static final String BATCH_ENDPOINT_NAME_1_0 = "/batchVocabularies";
+
+    public static final String GET_ALL_PATH_1_0 = API_VERSION_1_0 + ENDPOINT_NAME_1_0;
+    public static final String GET_ITEM_PATH_1_0 = API_VERSION_1_0 + ENDPOINT_NAME_1_0 + "/{key}";
+    public static final String POST_ITEM_PATH_1_0 = API_VERSION_1_0 + ENDPOINT_NAME_1_0;
+    public static final String DELETE_ITEM_PATH_1_0 = API_VERSION_1_0 + ENDPOINT_NAME_1_0 + "/{key}";
+    public static final String POST_BATCH_ITEM_PATH_1_0 = API_VERSION_1_0 + BATCH_ENDPOINT_NAME_1_0;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VocabularyController.class);
 
     @Autowired
     AwsDynamoDbVocabulary awsDynamoDbVocabulary;
 
-    @GetMapping(path = "/vocabularies")
+    @GetMapping(path = GET_ALL_PATH_1_0)
     public List<Vocabulary> getAllVocabulary() {
         LOGGER.info("Get All Vocabulary");
+
         return awsDynamoDbVocabulary.getAllItems();
     }
 
-    @GetMapping(path = "/vocabularies/{key}")
+    @GetMapping(path = GET_ITEM_PATH_1_0)
     public Vocabulary getVocabulary(@PathVariable String key) {
         LOGGER.info("Getting Vocabulary for: " + key);
+
         return awsDynamoDbVocabulary.getVocabulary(key);
     }
 
-    @RequestMapping(path = "/vocabularies", method = {RequestMethod.POST, RequestMethod.PUT},
+    @RequestMapping(path = POST_ITEM_PATH_1_0, method = {RequestMethod.POST, RequestMethod.PUT},
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Vocabulary putVocabulary(@RequestBody Vocabulary vocabulary) {
         LOGGER.info("Putting new Vocabulary: " + vocabulary);
+
         awsDynamoDbVocabulary.putVocabulary(vocabulary);
         return awsDynamoDbVocabulary.getVocabulary(vocabulary.getK_vocab_id());
     }
 
-    @RequestMapping(path = "/batchVocabularies", method = {RequestMethod.POST, RequestMethod.PUT},
+    @RequestMapping(path = POST_BATCH_ITEM_PATH_1_0, method = {RequestMethod.POST, RequestMethod.PUT},
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public void putBatchVocabulary(@RequestBody List<Vocabulary> vocabularyList) {
         LOGGER.info("Putting batch of vocabulary: " + vocabularyList.size());
+
         awsDynamoDbVocabulary.putBatchItem(vocabularyList);
     }
 
-    @DeleteMapping(path = "/vocabularies/{key}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(path = DELETE_ITEM_PATH_1_0, produces = MediaType.APPLICATION_JSON_VALUE)
     public Vocabulary deleteVocabulary(@PathVariable String key) {
         LOGGER.info("Deleting Vocabulary with key: " + key);
+
         return awsDynamoDbVocabulary.deleteVocabulary(key);
     }
 }
